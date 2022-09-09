d8b9fca3d92f7cdd94384bc371b96b400b4caf89
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2022-09-09 10:13:08 -0500

    Setup CI with Github actions (#7)
    * Created the CI workflow
    
    * Added dialyzer and credo
    
    * Format and mix.lock updated
    
    * Added fixes for the CI to pass
    
    * Updated CI workflow

--- lib/mintacoin_web/telemetry.ex
+++ lib/mintacoin_web/telemetry.ex
@@ -1,4 +1,6 @@
 defmodule MintacoinWeb.Telemetry do
+  @moduledoc false
+
   use Supervisor
   import Telemetry.Metrics
 
--- mix.exs
+++ mix.exs
@@ -5,12 +5,16 @@ defmodule Mintacoin.MixProject do
     [
       app: :mintacoin,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:mintacoin, :ex_unit],
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -42,7 +46,9 @@ defmodule Mintacoin.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 

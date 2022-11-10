abb2f2f2c9c8161b9d27d11a2136cb3accf29165
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2022-11-10 11:22:51 -0500

    Prepare for release v0.3.2 (#119)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mintacoin.MixProject do
   def project do
     [
       app: :mintacoin,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
